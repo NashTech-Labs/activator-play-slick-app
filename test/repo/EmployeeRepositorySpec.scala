1683ebd6f78faed473839e2cdd9188f099571628
Satendra kumar <satendra@knoldus.com>
2016-01-11 00:47:00 +0530

    Removed duplicate code from EmployeeRepositorySpec

--- test/repo/EmployeeRepositorySpec.scala
+++ test/repo/EmployeeRepositorySpec.scala
@@ -6,6 +6,7 @@ import play.api.test.WithApplication
 import scala.concurrent.duration.Duration
 import scala.concurrent.Await
 import java.util.Date
+import scala.concurrent.Future
 
 class EmployeeRepositorySpec extends Specification {
 
@@ -16,32 +17,33 @@ class EmployeeRepositorySpec extends Specification {
     def empRepo(implicit app: Application) = Application.instanceCache[EmployeeRepository].apply(app)
 
     "get all rows" in new WithApplication {
-      val result = Await.result(empRepo.getAll, Duration.Inf)
+      val result = await(empRepo.getAll)
       result.length === 1
       result.head.name === "test"
     }
 
     "insert a row" in new WithApplication {
-      val knolId = Await.result(empRepo.insert(Employee("sky", "sky@knoldus.com", new Date, "knoldus")), Duration.Inf)
+      val knolId = await(empRepo.insert(Employee("sky", "sky@knoldus.com", new Date, "knoldus")))
       knolId === 2
     }
 
     "insert multiple rows" in new WithApplication {
       val result = empRepo.insertAll(List(Employee("sky1", "sky1@knoldus.com", new Date, "knoldus"), Employee("sky2", "sky2@knoldus.com", new Date, "knoldus")))
-      val knolIds = Await.result(result, Duration.Inf)
+      val knolIds = await(result)
       knolIds === Seq(2, 3)
     }
 
     "update a row" in new WithApplication {
-      val result = Await.result(empRepo.update(Employee("sky", "sky@knoldus.com", new Date, "knoldus", Some(1))), Duration.Inf)
+      val result = await(empRepo.update(Employee("sky", "sky@knoldus.com", new Date, "knoldus", Some(1))))
       result === 1
     }
 
     "delete a row" in new WithApplication {
-      val result = Await.result(empRepo.delete(1), Duration.Inf)
+      val result = await(empRepo.delete(1))
       result === 1
     }
-
   }
+
+  def await[T](v: Future[T]): T = Await.result(v, Duration.Inf)
 
 }
