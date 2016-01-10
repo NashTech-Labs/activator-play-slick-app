eaa0263604e36512e3a1ac274ae61fa38237e02c
Satendra kumar <satendra@knoldus.com>
2016-01-10 19:22:18 +0530

    Unit test Update
    Unit test for Employee repo has been written.

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,6 +1,6 @@
 # --- !Ups
 CREATE TABLE "employee"("id" int PRIMARY KEY auto_increment,"name" varchar(200) , "email" varchar(200) , "dob" date ,"company_name" varchar);
-
+INSERT INTO "employee" values (1,'test', 'test@knoldus.com','2016-10-12','knoldus');
 # --- !Downs
 
 DROP TABLE "employee";
--- test/repo/EmployeeRepositorySpec.scala
+++ test/repo/EmployeeRepositorySpec.scala
@@ -13,13 +13,35 @@ class EmployeeRepositorySpec extends Specification {
 
   "Employee repository" should {
 
-    def empRepo(implicit app: Application) =
-      Application.instanceCache[EmployeeRepository].apply(app)
+    def empRepo(implicit app: Application) = Application.instanceCache[EmployeeRepository].apply(app)
+
+    "get all rows" in new WithApplication {
+      val result = Await.result(empRepo.getAll, Duration.Inf)
+      result.length === 1
+      result.head.name === "test"
+    }
 
     "insert a row" in new WithApplication {
-      val knolId = Await.result(empRepo.insert(Employee("sky", "knoldus", new Date, "")), Duration.Inf)
-      knolId === 1
+      val knolId = Await.result(empRepo.insert(Employee("sky", "sky@knoldus.com", new Date, "knoldus")), Duration.Inf)
+      knolId === 2
+    }
+
+    "insert multiple rows" in new WithApplication {
+      val result = empRepo.insertAll(List(Employee("sky1", "sky1@knoldus.com", new Date, "knoldus"), Employee("sky2", "sky2@knoldus.com", new Date, "knoldus")))
+      val knolIds = Await.result(result, Duration.Inf)
+      knolIds === Seq(2, 3)
+    }
+
+    "update a row" in new WithApplication {
+      val result = Await.result(empRepo.update(Employee("sky", "sky@knoldus.com", new Date, "knoldus", Some(1))), Duration.Inf)
+      result === 1
+    }
+
+    "delete a row" in new WithApplication {
+      val result = Await.result(empRepo.delete(1), Duration.Inf)
+      result === 1
     }
 
   }
+
 }
